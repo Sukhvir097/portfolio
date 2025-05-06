5a25221eb36027e57812e9d2cf91d0085bc1a461
sukhvir singh <sukhvirsingh4898@gmail.com>
2025-05-06 12:04:07 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
